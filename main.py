485154cd8cf5d2371d49bd1313e48c38a1ab241a
Trevor <shawtrevor97@yahoo.com>
2022-11-01 20:18:38 -0700

    [Feature] Incorporate a SQL database and implement CRUD operations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn 
 
 import uvicorn
 
@@ -11,6 +12,10 @@ app = FastAPI()
 
 app.include_router(user_router, prefix='/user')
 app.include_router(event_router, prefix="/event")
+
+@app.on_event("startup")
+def on_startup():
+    conn()
 
 
 if __name__ == "__main__": 
--- routes/events.py
+++ routes/events.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Body, HTTPException, status 
+from fastapi import APIRouter, Body, HTTPException, status, Depends
+from sqlmodel import select
 from models.events import Event 
+from database.connection import get_session
+from models.events import Event, EventUpdate
 from typing import List 
 
 event_router = APIRouter(
@@ -9,14 +12,21 @@ event_router = APIRouter(
 events = []
 
 @event_router.get('/', response_model= List[Event])
-async def retrieve_all_events() -> List[Event]:
+async def retrieve_all_events(session=Depends(get_session)) -> List[Event]:
+    statement = select(Event)
+    events = session.exec(statement).all()
     return events 
+    
 
-@event_router.get('/{id}', response_model=List[Event])
-async def retrieve_event(id: int) -> Event: 
-    for event in events: 
-        if event.id == id: 
-            return event 
+@event_router.get('/{id}', response_model=Event)
+async def retrieve_event(id: int, session=Depends(get_session)) -> Event: 
+    event = session.get(Event, id)
+    if event: 
+        return event 
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail = "Event with supplied ID does not exist"
+    )
     
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, 
@@ -24,19 +34,25 @@ async def retrieve_event(id: int) -> Event:
     )
     
 @event_router.post('/new')
-async def create_event(body: Event = Body(...)) -> dict: 
-    events.append(body)
+async def create_event(new_event: Event, session=Depends(get_session)) -> dict: 
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
+    
     return {
         "message": "Event created successfully"
     }
     
-@event_router.delete('/{id}')
-async def delete_event(id: int) -> dict: 
-    for event in events: 
-        if event.id == id: 
-            events.remove(event)
-            return {
-                "message": "Event deleted successfully"
+    
+@event_router.delete('/delete/{id}')
+async def delete_event(id: int, session=Depends(get_session)) -> dict: 
+    event = session.get(Event, id)
+    if event: 
+        session.delete(event)
+        session.commit()
+        
+        return {
+            "message": "Event deleted successfully"
             }
     
     raise HTTPException(
@@ -50,3 +66,22 @@ async def delete_all_events() -> dict:
     return {
         "message": "Events deleted successfully"
     }
+    
+    
+@event_router.put("/edit/{id}", response_model=Event)
+async def update_event(id:int, new_data: EventUpdate, session=Depends(get_session)) -> Event: 
+    event = session.get(Event, id)
+    if event: 
+        event_data = new_data.dict(exclude_unset=True)
+        for key, value in event_data.items():
+            setattr(event, key, value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+        
+        return event 
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail="Event with suppluied ID does not exist"
+    )
